7798eb8eaf2a1b20870819e3a28bead0da2d5e8f
unknown <molni.a.zp@gmail.com>
2023-10-26 18:39:39 +0200

    done 4 lessons

--- src/route/index.js
+++ src/route/index.js
@@ -4,6 +4,32 @@ const express = require('express')
 const router = express.Router()
 
 // ================================================================
+var header = {
+  name: {
+    firstname: 'Ivan',
+    lastname: 'Ivanov',
+  },
+  position: 'Junior Fullstack JS Developer',
+  salary: '600$ в місяць',
+  address: 'Poland, Warszawa',
+}
+
+var footer = {
+  social: {
+    email: {
+      text: 'ivanov@mail.com',
+      href: 'mailto:dmytro@mail.com',
+    },
+    phone: {
+      text: '+380670000123',
+      href: 'tel:+380670000123',
+    },
+    linkedin: {
+      text: 'LinkedIn',
+      href: 'https://www.linkedin.com/in/dmytro-test',
+    },
+  },
+}
 
 // router.get Створює нам один ентпоїнт
 
@@ -23,6 +49,192 @@ router.get('/summary', function (req, res) {
   //             ↙ cюди вводимо назву файлу з сontainer
   res.render('summary', {
     // ↙ сюди вводимо JSON дані
+
+    page: {
+      title: 'Resume | Summary',
+    },
+
+    header,
+    main: {
+      summary: {
+        title: 'Summary',
+        text: `Open-minded for new technologies, with 1 years of experience in development. Whenever I start to
+      work on a new project I learn the domain and try to understand the idea of the project. Good team
+      player, every colleague is a friend to me.`,
+      },
+
+      experience: {
+        title: 'Other experience',
+        text: `Pet project for parsing sport betting data from different platforms ( odds ) and sport statistics (
+                    tournament position, goals etc), analyzing by simple mathematics models and preparing probability
+                    for such events like: money line - first win / draw / second win, totals etc.`,
+      },
+    },
+    footer,
+  })
+})
+
+router.get('/skills', function (req, res) {
+  //             ↙ cюди вводимо назву файлу з сontainer
+  res.render('skills', {
+    // ↙ сюди вводимо JSON дані
+
+    page: {
+      title: 'Resume | Skills',
+    },
+
+    header,
+    main: {
+      skills: [
+        {
+          name: 'HTML',
+          point: 10,
+          isTop: true,
+        },
+        {
+          name: 'Handelbars',
+          point: 10,
+          isTop: true,
+        },
+        {
+          name: 'VS Code',
+          point: '8',
+          isTop: false,
+        },
+        {
+          name: 'Terminal',
+          point: 6,
+        },
+        {
+          name: 'React.js',
+          point: 0,
+        },
+        {
+          name: 'PHP',
+          point: null,
+        },
+      ],
+
+      hobbies: [
+        {
+          name: 'Велотуризм',
+          isMain: true,
+        },
+        {
+          name: 'Читання',
+          isMain: true,
+        },
+        {
+          name: 'Малювання',
+          isMain: false,
+        },
+      ],
+    },
+    footer,
+  })
+})
+
+router.get('/education', function (req, res) {
+  res.render('education', {
+    page: {
+      title: 'Resume | Education',
+    },
+
+    header,
+    main: {
+      education: [
+        {
+          name: 'Дитячий сад',
+          isEnd: true,
+        },
+        {
+          name: 'Гімназія',
+          isEnd: true,
+        },
+        {
+          name: 'Поліграфічний коледж',
+          isEnd: true,
+        },
+        {
+          name: 'Національний університет',
+          isEnd: false,
+        },
+      ],
+
+      certificates: [
+        {
+          name: 'Спеціаліст',
+          id: 1258,
+        },
+        {
+          name: 'Бакалавр',
+          id: 6584,
+        },
+        {
+          name: 'Магистр',
+          id: 3845,
+        },
+      ],
+    },
+
+    footer,
+  })
+})
+
+router.get('/work', function (req, res) {
+  res.render('work', {
+    layout: 'big',
+    page: {
+      title: 'Resume | Work',
+    },
+
+    header,
+    main: {
+      works: [
+        {
+          position: 'Junior Fullstack Developer',
+          company: {
+            name: 'IT Brains',
+            url: 'https://it-brains.com.ua/fullstack/',
+          },
+          duration: {
+            from: '05.07.2022',
+            to: null,
+          },
+          projectAmount: 3,
+          projects: [
+            {
+              name: 'Resume',
+              url: 'https://resume.com.ua/fullstack/',
+              about: 'Application for searching apartments',
+              stacks: [
+                {
+                  name: 'React.js',
+                },
+                {
+                  name: 'HTML/CSS',
+                },
+                {
+                  name: 'Node.js',
+                },
+              ],
+              awards: [
+                {
+                  name: 'oijvo;aifj',
+                },
+                {
+                  name: 'kjcnsoinfow',
+                },
+              ],
+              stacksAmount: 4,
+              awardsAmount: 5,
+            },
+          ],
+        },
+      ],
+    },
+
+    footer,
   })
 })
 
